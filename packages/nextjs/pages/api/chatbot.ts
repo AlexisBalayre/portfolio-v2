0dd1d85808bc7eaee8c319a4a2b09c31f6131c93
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-11-26 23:53:36 +0000

    Added a chatbox
    Signed-off-by: Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>

--- packages/nextjs/pages/api/chatbot.ts
+++ packages/nextjs/pages/api/chatbot.ts
@@ -2,130 +2,65 @@ import natural from "natural";
 import type { NextApiRequest, NextApiResponse } from "next";
 import chatbot from "~~/public/assets/data/chatbot.json";
 
+const tokenizer = new natural.WordTokenizer();
+const stemmer = natural.LancasterStemmer;
+const analyser = new natural.SentimentAnalyzer("English", stemmer, "afinn");
+
+// Fonction pour le traitement du texte
+function processText(text: string) {
+  const replacements: { [key: string]: string } = {
+    "'m": " am",
+    "'s": " is",
+    "'re": " are",
+    "'ll": " will",
+    "'ve": " have",
+    z: "s",
+    specialities: "specialisations",
+    best: "favourite",
+    alexis: "he",
+    she: "he",
+    they: "he",
+    them: "he",
+    their: "his",
+    theirs: "his",
+    its: "his",
+    "at the moment": "now",
+    favorite: "favourite",
+  };
+
+  // Remplacer en utilisant une expression régulière
+  const regex = new RegExp(Object.keys(replacements).join("|"), "gi");
+  text = text.toLowerCase().replace(regex, matched => replacements[matched]);
+
+  // Tokenize et Stem
+  const tokens = tokenizer.tokenize(text);
+  return tokens?.map(stemmer.stem);
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Get the user prompt
-  let { prompt } = req.body;
+  const { prompt } = req.body;
+  const promptProcessed = processText(prompt);
 
-  // Create a stemmer and sentiment analyser
-  const tokenizer = new natural.WordTokenizer();
-  const stemmer = natural.LancasterStemmer; // PorterStemmer is a stemming algorithm for English
-  const analyser = new natural.SentimentAnalyzer("English", stemmer, "afinn"); // AFINN is a list of English words rated for valence with an integer between minus five (negative) and plus five (positive).
-
-  // Everything to lowercase
-  prompt = prompt.toLowerCase();
-  // Replace words in the user prompt
-  const replacements = [
-    {
-      word: "'m",
-      replacement: " am",
-    },
-    {
-      word: "'s",
-      replacement: " is",
-    },
-    {
-      word: "'re",
-      replacement: " are",
-    },
-    {
-      word: "'ll",
-      replacement: " will",
-    },
-    {
-      word: "'ve",
-      replacement: " have",
-    },
-    {
-      word: "z",
-      replacement: "s",
-    },
-    {
-      word: "specialities",
-      replacement: "specialisations",
-    },
-    {
-      word: "best",
-      replacement: "favourite",
-    },
-    {
-      word: "alexis",
-      replacement: "he",
-    },
-    {
-      word: "she",
-      replacement: "he",
-    },
-    {
-      word: "they",
-      replacement: "he",
-    },
-    {
-      word: "them",
-      replacement: "he",
-    },
-    {
-      word: "their",
-      replacement: "his",
-    },
-    {
-      word: "theirs",
-      replacement: "his",
-    },
-    {
-      word: "at the moment",
-      replacement: "now",
-    },
-    {
-      word: "favorite",
-      replacement: "favourite",
-    },
-  ];
-  replacements.forEach(replacement => {
-    prompt = prompt.replace(replacement.word, replacement.replacement);
-  });
-  // Tokenize the user prompt
-  const promptTokenizedData = tokenizer.tokenize(prompt); // Tokenization is the process of breaking a stream of text up into words, phrases, symbols, or other meaningful elements called tokens.
-  // Stem the user prompt
-  const promptTokenizedStemData = promptTokenizedData?.map(natural.PorterStemmer.stem);
-  // Compute the sentiment of the user prompt
-  const promptSentiment = analyser.getSentiment(promptTokenizedStemData!);
-
-  // Initialize the max score and the best match
   let maxScore = 0;
-  let bestMatch = "I'm sorry, I can't answer to that yet."; // Default answer
+  let bestMatch = "I'm sorry, I can't answer to that yet.";
 
-  // Loop through the chatbot data and find the best match
   chatbot.forEach(pair => {
-    // Tokenize the question
-    const questionTokenizedData = tokenizer.tokenize(pair.question.toLowerCase()); // Tokenization is the process of breaking a stream of text up into words, phrases, symbols, or other meaningful elements called tokens.
-    // Stem the question
-    const questionTokenizedStemData = questionTokenizedData?.map(natural.PorterStemmer.stem);
-    // Compare the similarity between the question and the user prompt
+    const questionProcessed = processText(pair.question);
+
     const score = natural.JaroWinklerDistance(
-      promptTokenizedStemData?.join(" ") || "",
-      questionTokenizedStemData?.join(" ") || "",
-      {},
+      promptProcessed?.join(" ") || "",
+      questionProcessed?.join(" ") || "",
+      {}, // Add empty options object as the third argument
     );
-    // Get the sentiment of the question
-    const questionSentiment = analyser.getSentiment(questionTokenizedStemData!);
-    // Compare the sentiment of the question and the user prompt
-    // Considérer null comme neutre (0)
-    const promptSentimentNormalized = promptSentiment !== null ? promptSentiment : 0;
-    const questionSentimentNormalized = questionSentiment !== null ? questionSentiment : 0;
+
+    const promptSentiment = analyser.getSentiment(promptProcessed as string[]);
+    const questionSentiment = analyser.getSentiment(questionProcessed as string[]);
     const isSamePolarity =
-      (promptSentimentNormalized >= 0 && questionSentimentNormalized >= 0) ||
-      Math.abs(promptSentimentNormalized - questionSentimentNormalized) < 0.3;
-    // If the score is higher than the max score and the sentiment is the same, update the max score and the best match
-    console.log(
-      questionSentiment,
-      promptSentiment,
-      score,
-      pair.answer,
-      Math.abs(promptSentimentNormalized - questionSentimentNormalized),
-    );
+      (promptSentiment >= 0 && questionSentiment >= 0) || (promptSentiment < 0 && questionSentiment < 0);
+
     if (score > maxScore && score > 0.8 && isSamePolarity) {
-      maxScore = score; // Update the max score
-      bestMatch = pair.answer; // Update the best match
+      maxScore = score;
+      bestMatch = pair.answer;
     }
   });
 
