3028b949f2343e0bb52c996419b34a9ab9d4aa84
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-11-27 10:30:12 +0000

    Added a chatbot - Test in progress
    Signed-off-by: Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>

--- packages/nextjs/pages/api/chatbot.ts
+++ packages/nextjs/pages/api/chatbot.ts
@@ -68,7 +68,3 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   res.status(200).json({ response: bestMatch });
 }
-
-export const config = {
-  runtime: "edge",
-};
