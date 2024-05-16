366c0ceb15038d9dcee51f9e2e0f28b46c4e6130
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2024-05-16 16:55:01 +0200

    Updated metadata
    Signed-off-by: Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -4,29 +4,29 @@ import Head from "next/head";
 export const MetaHeader = () => {
   return (
     <Head>
-      <title>Alexis Balayre: Expert Data Engineer & Blockchain Developer</title>
+      <title>Alexis Balayre: AI Engineer & Blockchain Specialist</title>
       <meta charSet="utf-8" />
       <meta
         name="description"
-        content="Find out more about Alexis Balayre's career as a Data engineer and Blockchain developer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
+        content="Find out more about Alexis Balayre's career as an AI engineer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
       />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Alexis Balayre - Data Engineer | Blockchain Developer" />
-      <meta property="og:title" content="Alexis Balayre - Data Engineer | Blockchain Developer" />
+      <meta property="og:site_name" content="Alexis Balayre - AI Engineer" />
+      <meta property="og:title" content="Alexis Balayre - AI Engineer" />
       <meta
         property="og:description"
-        content="Find out more about Alexis Balayre's career as a Data engineer and Blockchain developer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
+        content="Find out more about Alexis Balayre's career as an AI engineer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
       />
       <meta property="og:image" content="https://alexis.balayre.com/preview.png" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:description"
-        content="Find out more about Alexis Balayre's career as a Data engineer and Blockchain developer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
+        content="Find out more about Alexis Balayre's career as an AI engineer. Specialising in Web3 and AI technologies, Alexis transforms complex data into usable information. Explore his projects and skills today."
       />
       <meta name="twitter:image" content="https://alexis.balayre.com/preview.png" />
       <meta
         name="keywords"
-        content="Alexis Balayre, French, Data Engineering Expert, Blockchain Development Specialist, Machine Learning, AI, Web3 Technologies, Smart Contracts Developer, Ethereum Blockchain, Data Analytics Engineer, Cryptocurrency Technology, Decentralized Applications (DApps), ISEP, Cranfield University, Paris"
+        content="Alexis Balayre, Data, Artificial Intelligence, AI, LLM, Intelligence Artificielle, IA, Machine Learning, ISEP, Cranfield University, Paris"
       />
       <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
